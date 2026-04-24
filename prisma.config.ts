0ba3ed664c99d16ef16daf0b5042c1bf7895c481
SAGAR KUMAR DAS <sagarkumardas306400@gmail.com>
2026-04-24 18:38:55 +0530

    Tested DB connection and tested pushing data to db

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,19 @@
+// import { defineConfig } from "prisma/config";
+
+// export default defineConfig({
+//   datasource: {
+//     db: {
+//       url: process.env.DATABASE_URL!,
+//     },
+//   },
+// });
+
+import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
+  schema: "./prisma/schema.prisma",
   datasource: {
-    db: {
-      url: process.env.DATABASE_URL!,
-    },
+    url: process.env.DATABASE_URL,
   },
 });
